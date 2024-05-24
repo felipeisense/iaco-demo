9dd1a66038138dd8ccc5ad83862a6b7bdcf3c914
felipeisense <felipe@isense.cl>
2024-05-24 17:52:28 -0400

    define rute /

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"; // Marca el archivo como un componente del lado del cliente
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
+
+  return null;
+}
